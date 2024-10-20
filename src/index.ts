daf5f4c2380f90c6a4be609653d1236b15bd5eed
Edward <e.arechwa@gmail.com>
2024-10-20 12:04:35 +0200

    ft: implement base api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('index 2')
+import http, { IncomingMessage, ServerResponse } from 'http'
+import { jsonResponse } from './utils/json-response'
+
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const method = req.method
+    const url = req.url
+
+    switch (url) {
+      case '/':
+        jsonResponse(res, 200, { message: 'Welcome to the API!' })
+        break
+      default:
+        jsonResponse(res, 404, { error: 'Route not found' })
+        break
+    }
+  }
+)
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
